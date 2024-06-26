b69e8d0de2973e4312971d0031309651fb6505a9
amiruddin29 <A187996@siswa.ukm.edu.my>
2024-06-27 03:35:01 +0800

    Update about.py

--- about.py
+++ about.py
@@ -80,8 +80,8 @@ def app():
     with tabs[6]:
         st.subheader('E-book')
         pdf_file = 'stocksinfo.pdf'  
-        pdf_viewer(pdf_file)
-        pdf_display = f'<iframe src="{pdf_viewer}" width="1000" height="1000" type="application/pdf"></iframe>'
+        #pdf_viewer(pdf_file)
+        pdf_display = f'<iframe src="{pdf_file}" width="1000" height="1000" type="application/pdf"></iframe>'
         st.markdown(pdf_display, unsafe_allow_html=True)
 
 if __name__ == "__main__":
